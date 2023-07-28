030226361d61a59082b004434f89bf80fa0cd7a9
Kevin Peralta <76978772+realKP@users.noreply.github.com>
2023-07-27 18:35:20 -0700

    Added edit and delete front end for members

--- library_admin/library_app/urls.py
+++ library_admin/library_app/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     # members page
     path("members/", views.MembersView.as_view(), name="members"),
     # add member
-    path("members/add_member", views.get_member_info, name="add-member"),
+    # path("members/add_member", views.get_member_info, name="add-member"),
     # ex: /9781594133299
-    path("<str:isbn>/", views.detail, name="detail"),
-    # ex:
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex:
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    # path("<str:isbn>/", views.detail, name="detail"),
+    # # ex:
+    # path("<int:question_id>/results/", views.results, name="results"),
+    # # ex:
+    # path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- library_admin/library_app/views.py
+++ library_admin/library_app/views.py
@@ -15,45 +15,36 @@ def index(request):
 class MembersView(generic.ListView):
     template_name = "library_app/members.html"
     context_object_name = "members"
+    # queries for all members
     queryset = Member.objects.all()
 
+    def get_context_data(self, **kwargs):
+        # every request (GET or POST) receives blank form for modal
+        context = super().get_context_data(**kwargs)
+        form = MemberForm()
+        context["form"] = form
+        return context
 
-def get_member_info(request):
-    # if this is a POST request we need to process the form data
-    if request.method == "POST":
-        # create a form instance and populate it with data from the request:
+    def post(self, request, *args, **kwargs):
         form = MemberForm(request.POST)
-        # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
             form.save()
-            # redirect to a new URL:
-            return HttpResponseRedirect("/members/")
+            # redirect to members page to display new entry
+            return HttpResponseRedirect(reverse("library_app:members"))
 
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = MemberForm()
-    context = {"add-member-form": form}
-    return render(request, "library_app/add-member.html", context)
 
 ##########################################################
-def member(request):
-    latest_book_list = Book.objects.order_by("-isbn")[:5]
-    context = {
-        "latest_book_list": latest_book_list,
-    }
-    return render(request, "library_app/index.html", context)
+
+# def detail(request, isbn):
+#     book = get_object_or_404(Book, pk=isbn)
+#     return render(request, "library_app/detail.html", {"book": book})
 
 
-def detail(request, isbn):
-    book = get_object_or_404(Book, pk=isbn)
-    return render(request, "library_app/detail.html", {"book": book})
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
